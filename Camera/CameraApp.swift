1cdc2fbc4e6289cfab1b139023f7d6b870d0b424
Shohei <yukawa.shouhei@gmail.com>
2025-04-08 20:27:28 +0900

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by 湯川昇平 on 2025/04/08.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
