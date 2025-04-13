8feed1b19b5aeaec93d86daf31f068fffe59d9c3
Shohei <yukawa.shouhei@gmail.com>
2025-04-13 15:57:17 +0900

    CachedImageManagerの学習 途中

---
+++ Camera/CachedImageManager.swift
@@ -0,0 +1,18 @@
+//
+//  CachedImageManager.swift
+//  Camera
+//
+//  Created by 湯川昇平 on 2025/04/13.
+//
+
+import SwiftUI
+
+struct CachedImageManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CachedImageManager()
+}
